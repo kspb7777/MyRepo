5cda0d4f18a28bdbba932fc34c3f7d983aa5e4ac
kspb7777 <101615166+kspb7777@users.noreply.github.com>
2022-06-01 22:06:58 +0530

    Add files via upload

---
+++ subpackage.java/src/subpackage/gitsub.java
@@ -0,0 +1,12 @@
+package subpackage;
+
+public class gitsub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi This is Lal");
+
+	}
+
+}
